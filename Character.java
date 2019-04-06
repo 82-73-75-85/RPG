a9c4d0f889a89094e04ca4450a260a4827f7ecbb
Riku <47715082+82-73-75-85@users.noreply.github.com>
2019-04-06 15:04:24 +0900

    課題

--- Character.java
+++ Character.java
@@ -1,9 +1,34 @@
-public class Character{
-   int hp;
-   int mp;
-   String name;
+class Character{
+   private int hp;
+   private int mp;
+   private int atk;
+   private String name;
+
+   public Character(){
+      name = "unknown";
+      hp = 100;
+      mp = 80;
+      atk = 40;
+   }
+
+   public Character(int hp, int mp, int atk, String name){
+      this.name = name;
+      this.hp = hp;
+      this.mp = mp;
+      this.atk = atk;
+   }
 
    public void introduce() {
-      System.out.println("私は"+name+",hpは"+hp+",mpは"+mp+"だ！");
+      System.out.println("私は"+name+",体力は"+hp+",魔力は"+mp+",攻撃力は"+atk+"だ！");
+   }
+
+   private void damage(int atk, String name){
+      this.hp -= atk;
+      System.out.println(this.name+"は"+name+"から" + atk+"の攻撃を受けた！");
+   }
+
+   public void attack(Character c){
+      System.out.println(name +"の攻撃！");
+      c.damage(atk, name);
    }
 }
--- RPG.java
+++ RPG.java
@@ -1,18 +1,12 @@
 public class RPG{
    public static void main(String[] args) {
-      Character a = new Character();
-
-      a.hp = 100;
-      a.mp = 80;
-      a.name = "スバル";
-
-      Character b = new Character();
-
-      b.hp = 200;
-      b.mp = 130;
-      b.name = "エミリア";
+      Character a = new Character(100, 80, 40, "スバル");
 
       a.introduce();
+
+      Character b = new Character(200, 180, 80, "エミリア");
+
       b.introduce();
+      a.attack(b);
    }
 }
